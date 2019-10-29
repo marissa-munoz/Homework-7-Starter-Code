19ad9e4e9b6ae5ca3df9ade94dbd6a6a3f1634a6
Marissa Munoz <marissamunoz@ou.edu>
2019-10-29 12:41:24 -0500

    starter code, plus message

---
+++ Homework7Classes/Homework7Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//please remember to create the class, I did not create it for you!! - Marissa
+namespace Homework7Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
